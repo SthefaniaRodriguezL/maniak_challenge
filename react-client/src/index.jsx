44016118dac61f1e8a8e98704cc00f0fe90eddaf
SthefaniaRodriguezL <stherod13@gmail.com>
2019-04-17 17:47:42 -0500

    Checking API response, successful

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-
+ constructor() {
+   super()
+   this.state = {
+     data: []
+   }
+ }
+ componentDidMount() {
+   fetch("https://raw.githubusercontent.com/Bernabe-Felix/Bellotero/master/app.json")
+   .then(rsp => rsp.json())
+   .then(rsp => { this.setState({data: rsp})});
+ }
 
   render () {
+    console.log(this.state.data);
     return (<div>
       <h1> Maniak Challenge</h1>
 
