f7ed4d40e51e16968ab747ac15029f370fe8d3a3
SthefaniaRodriguezL <stherod13@gmail.com>
2019-04-19 13:46:37 -0500

    Adding text static

--- react-client/components/Calculator.jsx
+++ react-client/components/Calculator.jsx
@@ -16,8 +16,8 @@ class Calculator extends React.Component {
   render () {
     console.log(this.state.calculate);
     return (<div>
-      <h1> Calculator </h1>
-
+      <h1> Save more with Bellotero.io </h1>
+      <p> With Bellotero.io you save time and money make real-time decisions that boost your business and your botton line. Get less wrongfully blocked payments, save time on bookkeeping and no need to worry about safety. </p>
     </div>)
   }
 }
--- react-client/components/Testimonial.jsx
+++ react-client/components/Testimonial.jsx
@@ -16,7 +16,8 @@ class Testimonial extends React.Component {
   render () {
     console.log(this.state.stories);
     return (<div>
-      <h1> Calculator </h1>
+      <h1> Testimonial </h1>
+      <p>"It's funny what memory does, isn't it? My favorite holiday tradition might not have happened more than once or twice. But because it is such a good memory, so encapsulating of everything I love about the holidays, in my mind it happened every year. Without fail" </p>
 
     </div>)
   }
